d731c0ada6983995c5d136447da026a1e170564b
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2020-06-17 17:13:55 +1000

    fix: Actually get the workflow jobs...

--- src/index.ts
+++ src/index.ts
@@ -11,27 +11,21 @@ async function run(): Promise<void> {
       const githubToken = core.getInput('github-token', { required: true })
       core.setSecret(githubToken)
 
-      core.info('Gathered input!')
-
       const octokit = GitHub.getOctokit(githubToken)
 
-      core.info('Acquired octokit!')
-
       const context = GitHub.context
-
-      core.info(`${context.repo.owner}`)
-      core.info(`${context.repo.repo}`)
-      core.info(`${GITHUB_RUN_ID}`)
 
       let workflowJobs = octokit.actions.listJobsForWorkflowRun({
         owner: context.repo.owner,
         repo: context.repo.repo,
         run_id: parseInt(GITHUB_RUN_ID, 10)
-      }).catch(error => {
+      })
+      .then(response => {
+        return response.data.jobs
+      })
+      .catch(error => {
         core.setFailed(error.message)
       })
-
-      core.info('Listing this workflows Jobs:')
 
       core.info(JSON.stringify(workflowJobs))
     }
